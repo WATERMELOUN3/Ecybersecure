5fed3c0b401305fa00ced5cffa818b33ebec27f4
WATERMELOUN3 <alexis.gelin@outlook.fr>
2020-09-16 00:04:23 +0200

    Création de l'architecture du projet, et de la "db"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import { AppComponent } from './app.component';
+import { DialogComponent } from './dialog/dialog.component';
+import { ChoicesComponent } from './choices/choices.component';
+import { CharacterComponent } from './character/character.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { GameComponent } from './game/game.component';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent,
+    ChoicesComponent,
+    CharacterComponent,
+    NotificationsComponent,
+    GameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
