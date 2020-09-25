6f11a644d058398a30529c8ec02e6c2a050e4636
WATERMELOUN3 <alexis.gelin@outlook.fr>
2020-09-25 17:17:45 +0200

    More dialogs

--- src/app/dialog.service.ts
+++ src/app/dialog.service.ts
@@ -20,6 +20,52 @@ export class DialogService {
       startDialog: this.currentDialog,
     };
 
+    // Dialog (13)
+    // Continuer - dial 11
+    const dial13 = new Dialog();
+    dial13.message =
+      'Plus tard un colis chrono posté en mauvais état vous est livré avec des photos prises depuis votre webcam.';
+    dial13.choices = [
+      new Choice('Quitter', null),
+      new Choice('Chapitre suivant', null),
+    ];
+    dial13.mood = CharacterMood.Hello;
+
+    // Dialog (12)
+    // Continuer - dial 11
+    const dial12 = new Dialog();
+    dial12.message =
+      'La nuit passe et le lendemain matin, alors que vous êtes déjà en retard vous vous demandez si vous devez prendre la clef USB avec vous.';
+    dial12.choices = [
+      new Choice('Vous apportez la clef USB avec vous', null),
+      new Choice('Vous laissez la clef USB dans le placard', null),
+    ];
+    dial12.mood = CharacterMood.Idle;
+
+    // Dialog (11)
+    // Placard - dial 8
+    const dial11 = new Dialog();
+    dial11.message =
+      "Cette jolie clef USB va finir sa vie dans la poussière de votre placard... Qui sait, peut-être qu'un jour vos petits enfants trouverons la clef USB.";
+    dial11.choices = [new Choice('Continuer', dial12)];
+    dial11.mood = CharacterMood.Idle;
+
+    // Dialog (10)
+    // Professionnel - dial 7
+    const dial10 = new Dialog();
+    dial10.message =
+      "Une fois la clef USB branchée à votre ordinateur professionnel, rien ne sa passe. Vous en concluez qu'elle ne marche pas et vous la rangez dans un placard.";
+    dial10.choices = [new Choice('Continuer', dial12)];
+    dial10.mood = CharacterMood.Idle;
+
+    // Dialog (9)
+    // Personnel - dial 7
+    const dial9 = new Dialog();
+    dial9.message =
+      "Une fois la clef USB branchée à votre ordinateur personnel, rien ne sa passe. Vous en concluez qu'elle ne marche pas et vous la rangez dans un placard.";
+    dial9.choices = [new Choice('Continuer', dial13)];
+    dial9.mood = CharacterMood.Idle;
+
     // Dialog (4)
     // Jeter la clef - dial 2
     const dial4 = new Dialog();
@@ -37,7 +83,7 @@ export class DialogService {
     dial8.message = '';
     dial8.choices = [
       new Choice('Jeter la clef USB', dial4),
-      new Choice('Mettre la clef USB dans un placard', null),
+      new Choice('Mettre la clef USB dans un placard', dial11),
     ];
     dial8.mood = CharacterMood.Think;
 
@@ -49,9 +95,9 @@ export class DialogService {
     dial7.choices = [
       new Choice(
         'Brancher la clef USB sur votre ordinateur professionnel',
-        null
+        dial10
       ),
-      new Choice('Brancher la clef USB sur votre ordinateur personnel', null),
+      new Choice('Brancher la clef USB sur votre ordinateur personnel', dial9),
     ];
     dial7.mood = CharacterMood.Think;
 
